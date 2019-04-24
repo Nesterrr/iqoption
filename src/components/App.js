5af090e1e9b9914af0caefb05960cf9577f2d4ea
Svyatoslav nestreuk <s.nesteruk@pulkovo-service.ru>
2019-04-24 21:26:50 +0300

    wip

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import moment from 'moment';
+
+class App extends Component {
+    render() {
+        const monthDate = moment().startOf('month').daysInMonth();
+        const firstDay = moment(moment())
+            .startOf("month")
+            .format("d"); 
+
+        console.log('monthDate: ', monthDate);
+        console.log('firstDay: ', firstDay);
+        return (
+            <div>
+                <h1>My React App!</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App.js";
 
-
-
-class App extends Component {
-    render() {
-        return (
-            <div>
-                <h1>My React App!</h1>
-            </div>
-        );
-    }
-}
-
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
