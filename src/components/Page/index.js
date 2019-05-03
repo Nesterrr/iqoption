6cc3775ec9221bbf76b9be347aa8f2d34e51313a
Svyatoslav Nesteruk <2626246q@gmail.com>
2019-05-03 13:24:58 +0300

    delete useless return

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -14,32 +14,30 @@ type PropsType = {
 
 export const Page = ({
     days,
-}: PropsType): React.Element<*> => {
-    return (
-        <ul css={style.monthContainer}>
-            {
-                days.map(({
-                    dateString,
-                    number,
-                    partName
-                }: {
-                    dateString: string,
-                    number: number,
-                    partName: string
-                }, index: number): React.Element<typeof Day> => {
-                    const isToday = moment(dateString).isSame(moment().format("YYYY-MM-DD"));
-                    return (
-                        <Day
-                            key={`${dateString}-${number}-${index}`}
-                            number={number}
-                            dateString={dateString}
-                            isToday={isToday}
-                            dateString={dateString}
-                            partName={partName}
-                        />
-                    )
-                })
-            }
-        </ul>
-    );
-}
+}: PropsType): React.Element<*> => (
+    <ul css={style.monthContainer}>
+        {
+            days.map(({
+                dateString,
+                number,
+                partName
+            }: {
+                dateString: string,
+                number: number,
+                partName: string
+            }, index: number): React.Element<typeof Day> => {
+                const isToday = moment(dateString).isSame(moment().format("YYYY-MM-DD"));
+                return (
+                    <Day
+                        key={`${dateString}-${number}-${index}`}
+                        number={number}
+                        dateString={dateString}
+                        isToday={isToday}
+                        dateString={dateString}
+                        partName={partName}
+                    />
+                )
+            })
+        }
+    </ul>
+);
